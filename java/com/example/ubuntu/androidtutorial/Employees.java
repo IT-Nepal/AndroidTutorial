46b0e93c3564b0a3efe95fefb60f6876427dfb08
bucky <bucky123@gmail.com>
2018-08-19 19:15:25 +0545

    Create pojo class for maintaining all employee details

---
+++ java/com/example/ubuntu/androidtutorial/Employees.java
@@ -0,0 +1,17 @@
+package com.example.ubuntu.androidtutorial;
+
+import java.util.ArrayList;
+
+//create pojo class for maintaining all employee details.
+public class Employees {
+
+    private ArrayList<Employee> list;
+
+    public ArrayList<Employee> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Employee> list) {
+        this.list = list;
+    }
+}
