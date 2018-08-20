673e4994c7fd80fb09084d370f99e228eb2c5b4f
bucky <bucky123@gmail.com>
2018-08-20 14:29:48 +0545

    upto responding from the server

--- java/com/example/ubuntu/androidtutorial/SOAP_WSDL.java
+++ java/com/example/ubuntu/androidtutorial/SOAP_WSDL.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.ksoap2.transport.HttpsTransportSE;
+
 // Download jar file( ksoap2 android .jar
 // app ->New-> Module -> import .JAR/.AAR Package -> add jar file
 // app -> Open Module Setting -> Dependencies -> + -> Module Dependencies
@@ -22,6 +29,31 @@ public class SOAP_WSDL extends AppCompatActivity {
     }
 
     public void getDetail(View view){
+
+        String wsdl_url = "http://cxf.547215.n5.nabble.com/file/n5634307/ip2geo.wsdl";
+        String soap_action = "http://ws.cdyne.com/ResolveIP"; //n number of services choose particular one service.
+        String name_space = "http://ws.cdyne.com";
+        String method_name = "ResolveIP";
+
+        SoapObject soapObject = new SoapObject(name_space, method_name);
+        soapObject.addProperty("ipAddress", editText.getText().toString());
+
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11); //version of soap service
+//        if we are calling .Net web service then we have to use
+        envelope.dotNet = true;
+        envelope.setOutputSoapObject(soapObject);
+//        transferring request over the network
+
+        HttpTransportSE hts = new HttpTransportSE(wsdl_url);
+        try{
+            hts.call(soap_action, envelope);
+//            hit the server and get the response from server.
+
+            SoapObject object = (SoapObject) envelope.bodyIn;
+           String respon = object.getProperty(0).toString(); //get the response in zero index
+
+            System.out.println(respon);
+        }catch(Exception e){}
 
     }
 }
